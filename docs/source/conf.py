fce30cf0e2bf0967e0b3f3353b9c2d8432cef18f
Bryan D Esser <bryan.d.esser@gmail.com>
2025-04-21 13:43:47 +1000

    modified:   docs/source/conf.py 	modified:   docs/source/graphical_user_interface.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,9 +23,6 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx_rtd_theme",
 ]
-rst_prolog = """
-             .. include:: <s5defs.txt>
-             """
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54"
 
