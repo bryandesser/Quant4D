499399aac1178617e770bd9ee962b4e3bae65ee0
Bryan D Esser <bryan.d.esser@gmail.com>
2025-05-29 02:48:46 +0000

    May 29, 2025, 12:48 PM

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,11 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
     "sphinx_rtd_theme",
+    'sphinxcontrib.matlab',
+    'sphinx.ext.autodoc',
 ]
+
+primary_domain = "mat"
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54"
 
