f2cbe703d2c8f83d5df7247e19b201052d997057
Bryan D Esser <bryan.d.esser@gmail.com>
2025-08-19 11:55:22 +1000

    modified:   docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,12 +65,3 @@ html_context = {
    'github_repo': 'Quant4D',
    'conf_py_path': "/source/",
 }
-
-# generate README.rst for GitHub
-#file_list = ["index.rst", "installation.rst", "acknowledgement.rst"]
-#
-#with open(file = "../../README.rst", mode = "w") as outfile:
-#    for filename in file_list:
-#        with open(file = filename, mode = "r") as infile:
-#            outfile.write(infile.read().split("Contents\n^^^^^^^^")[0])
-#        outfile.write("\n\n")
