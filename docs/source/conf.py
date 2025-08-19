6b698f81e8efdc276a0bb40e505fe9b19b41060a
Bryan D Esser <bryan.d.esser@gmail.com>
2025-08-19 11:31:37 +1000

    modified:   docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,7 +69,7 @@ html_context = {
 # generate README.rst for GitHub
 file_list = ["/docs/source/index.rst", "/docs/source/installation.rst", "/docs/source/acknowledgement.rst"]
 
-with open(file = "/README.rst", mode = "w") as outfile:
+with open(file = "../../README.rst", mode = "w") as outfile:
     for filename in file_list:
         with open(file = filename, mode = "r") as infile:
             outfile.write(infile.read().split("Contents\n^^^^^^^^")[0])
