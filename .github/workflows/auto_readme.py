97953e23c34b7c8e27e125623f633574a7113c6e
Bryan D Esser <bryan.d.esser@gmail.com>
2025-08-19 12:35:53 +1000

    modified:   .github/workflows/auto_readme.py 	modified:   .github/workflows/auto_readme.yml

--- .github/workflows/auto_readme.py
+++ .github/workflows/auto_readme.py
@@ -4,5 +4,5 @@ file_list = ["./docs/source/index.rst", "./docs/source/installation.rst", "./doc
 with open(file = "./README.rst", mode = "w") as outfile:
     for filename in file_list:
         with open(file = filename, mode = "r") as infile:
-            outfile.write(infile.read().split("Contents\n^^^^^^^^")[0])
+            outfile.write(infile.read().split("Contents\n^^^^^^^^")[0].replace("../_static", "./docs/_static"))
         outfile.write("\n\n")
